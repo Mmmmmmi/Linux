0d0df89a72d22edb7290967b31f16af58ee0b8c3
812632545@qq.com <812632545@qq.com>
2018-10-09 21:39:26 +0800

    10.09

--- BinaryTree/BinaryTree.c
+++ BinaryTree/BinaryTree.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "BinaryTree.h"
+#include "Queue.h"
 
                                                       
 BNode* CreateNode(TDataType data)              
@@ -67,24 +68,149 @@ void PostOrderRec(BNode *root)
     printf("%d ", root->data);
 }
 
+int GetBTNodeSize(BNode *root)
+{
+    if (root == NULL) {
+        return 0;
+    }
+    return GetBTNodeSize(root->left) + GetBTNodeSize(root->right) + 1;
+}
+
+int GetBTNodeLeafSize(BNode *root)
+{
+    if (root == NULL) {
+        return 0;
+    }
+    else if  (root->left == NULL &&  root->right == NULL) {
+        return 1;
+    }
+    else {
+        return GetBTNodeLeafSize(root->left) + GetBTNodeLeafSize(root->right);
+    }
+}
+
+int GetKLevelSize(BNode *root, int k)
+{
+    assert(k > 0);
+    if (root == NULL) {
+        return 0;
+    }
+    if (k == 1) {
+        return 1;
+    }
+    return GetKLevelSize(root->left, k - 1) + GetKLevelSize(root->right, k - 1);
+}
+
+int GetBTNodeHeight(BNode *root)
+{
+    int left = 0;
+    int right = 0;
+    if (root == NULL) {
+        return 0;
+    }
+    left = GetBTNodeHeight(root->left);
+    right = GetBTNodeHeight(root->right);
+    return (left > right ? left :right) + 1;
+}
+
+BNode *SearchBTNode(BNode *root, TDataType key)
+{
+    BNode *ret = NULL;
+    if (root == NULL) {
+        return NULL;
+    }
+    if (root->data == key) {
+        return root;
+    }
+    ret = SearchBTNode(root->left, key);
+    if (ret) {
+        return ret;
+    }
+    ret = SearchBTNode(root->right, key);
+    return ret;
+}
+
+void LevelOrder(BNode *root)
+{
+    BNode *front = NULL;
+    Queue queue;
+    Queue_Initialize(&queue);
+    Queue_Push(&queue, root);
+    while(!Queue_Empty(&queue)) {
+        front = Queue_Top(&queue);
+        Queue_Pop(&queue);
+        printf("%d  ", front->data);
+        if (front->left != NULL) {
+            Queue_Push(&queue, front->left);
+        }
+        if (front->right != NULL) {
+            Queue_Push(&queue, front->right);
+        }
+    }
+    Queue_Destroy(&queue);
+}
+
+int IsCompleteTree(BNode *root)
+{
+    int flag = 1;
+    BNode *front = NULL;
+    Queue queue;
+    if (root == NULL) {
+        return 1;
+    }
+    Queue_Initialize(&queue);
+    Queue_Push(&queue, root);
+    while(!Queue_Empty(&queue)) {
+        front = Queue_Top(&queue);
+        Queue_Pop(&queue);
+        if (front != NULL && flag == 0) {
+            printf("No\n");
+            Queue_Destroy(&queue);
+            return 0;
+        }
+        if (front == NULL) {
+            flag = 0;
+            continue;
+        }
+        Queue_Push(&queue, front->left);
+        Queue_Push(&queue, front->right);
+    }
+    Queue_Destroy(&queue);
+    printf("Is\n");
+    return 1;
+}
 
 
 void  BinaryTreeTest()                                
-{                                                     
+{ 
+    int key = 0;
     TDataType preorder[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6 ,7};                         
     int size = sizeof(preorder) / sizeof(preorder[0]);
     BNode *btree = NULL;
     Result result = {NULL, 0};
     result = CreateTree(preorder, size);
     btree = result.result;
-    printf("PreOrderRec    :");
+    printf("PreOrderRec          :");
     PreOrderRec(btree);
     printf("\n");
-    printf("InOrderRec     :");
+    printf("InOrderRec           :");
     InOrderRec(btree);
     printf("\n");
-    printf("PostOrderRec   :");
+    printf("PostOrderRec         :");
     PostOrderRec(btree);
+    printf("\n");
+    printf("GetBTNodeSize        :%d \n", GetBTNodeSize(btree));
+    printf("GetBTNodeLeafSize    :%d \n", GetBTNodeLeafSize(btree));
+    key = 3;   
+    printf("GetKLevelSize  %d     :%d \n", key, GetKLevelSize(btree, key));
+    printf("GetBTNodeHeight      :%d \n", GetBTNodeHeight(btree));
+    key = 8;
+    printf("SearchBTNode   %d     :%p \n", key, SearchBTNode(btree, key));
+    printf("LevelOrder           :");
+    LevelOrder(btree); 
+    printf("\n");
+    printf("IsCompleteTree       :");
+    IsCompleteTree(btree);
     printf("\n");
 }                      
 
--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -19,4 +19,11 @@ typedef struct Result {
 
 Result CreateTree(TDataType preorder[], int size);  //创建树
 void PreOrderRec(BNode *root);          //先序遍历
+void InOrderRec(BNode *root);           //中序遍历
+void PostOrderRec(BNode *root);         //后序遍历 
+int GetBTNodeSize(BNode *root);         //获取节点个数
+int GetBTNodeLeafSize(BNode *root);     //获取叶子节点个数
+int GetKLevelSize(BNode *root, int k);  //获取第K层节点个数
+int GetBTNodeHeight(BNode *root);       //获取深度
+BNode *SearchBTNode(BNode *root, TDataType key);    //查找节点
 void BinaryTreeTest();     //测试
