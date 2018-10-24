5d63eddc721116f48827d99bada5653e664b179b
Mmmmmmi <812632545@qq.com>
2018-10-24 23:10:58 +0800

    10.24

--- InterView/FindMedianSortedArrays.c
+++ InterView/FindMedianSortedArrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 double FindMedianSortedArrays(int *nums1, int nums1Size, int* nums2, int nums2Size)
 {
     double ret = 0;
@@ -7,13 +8,15 @@ double FindMedianSortedArrays(int *nums1, int nums1Size, int* nums2, int nums2Si
     int flag2 = (num + 2) / 2;
     int val1 = 0;
     int val2 = 0;
-    int cur1 = 1;
-    int cur2 = 1;
+    int num1 = 0;   //增序列或减
+    int num2 = 0;
+    int now = 0;
     int tmp1 = 0;
     int tmp2 = 0;
+    int cur1 = 0;
+    int cur2 = 0;
     int tmp = 0;
-    int now = 1;
-    if (nums1Size == 0 || nums2Size == 0) {
+    if (nums1Size == 0 || nums2Size == 0) {    //有一个数组为空
         if (nums1Size == 0) {
             val1 = nums2[flag1 - 1];
             val2 = nums2[flag2 - 1];
@@ -24,51 +27,83 @@ double FindMedianSortedArrays(int *nums1, int nums1Size, int* nums2, int nums2Si
         }
 
     } 
-    else {
-        while (cur1 <= nums1Size && cur2 <= nums2Size && now <= flag2) {
-            tmp1 = nums1[cur1 - 1];
-            tmp2 = nums2[cur2 - 1];
-            tmp = tmp1 >= tmp2 ? tmp2 : tmp1;
-            if (now == flag1) {
-                val1 = tmp;
-                now++;
+    else {      //两个都不为空 
+            num1 = nums1[0] >= nums1[nums1Size - 1] ? 1 : 0;
+            num2 = nums2[0] >= nums2[nums2Size - 1] ? 1 : 0;
+            if (num1 == 1) {    //第一个为增数组
+                if (num2 == 1) {
+                    cur1 = 1;
+                    cur2 = 1;
+                    while (cur1 <= nums1Size && now <= flag2) {
+                        now++;
+                        tmp = nums1[cur1 - 1]; 
+                        if (cur2 <= nums2Size && nums2[cur2 - 1] <= nums1[cur1 - 1]) {
+                            tmp = nums2[cur2 - 1];
+                            cur2++;
+                            cur1--;
+                        }
+                        cur1++;
+                        if (now == flag1) {
+                            val1 = tmp;
+                        }
+                        if (now == flag2) {
+                            val2 = tmp;
+                        }
+                    }
+                    if (cur2 <= nums1Size && now <= flag2) {
+                        now++;
+                        tmp = nums1[cur2 - 1];
+                        cur2++;
+                        if (now == flag1) {
+                            val1 = tmp;
+                        }
+                        if (now == flag2) {
+                            val2 = tmp;
+                        }
+                    }
+                }
+                else {
+                    cur1 = 1;
+                    cur2 = nums2Size;
+                    while (cur1 <= nums1Size && now <= flag2) {
+                        now++;
+                        tmp = nums1[cur1 - 1]; 
+                        if (cur2 >= 1 && nums2[cur2 - 1] <= nums1[cur1 - 1]) {
+                            tmp = nums2[cur2 - 1];
+                            cur2--;
+                            cur1--;
+                        }
+                        cur1++;
+                        if (now == flag1) {
+                            val1 = tmp;
+                        }
+                        if (now == flag2) {
+                            val2 = tmp;
+                        }
+                    }
+                    if (cur2 <= nums1Size && now <= flag2) {
+                        now++;
+                        tmp = nums1[cur2 - 1];
+                        cur2++;
+                        if (now == flag1) {
+                            val1 = tmp;
+                        }
+                        if (now == flag2) {
+                            val2 = tmp;
+                        }
+                }
             }
-            printf("cur1 = %d cur2 = %d now = %d val1 = %d val2 = %d\n", cur1, cur2, now, val1, val2);
-            if (now == flag2) {
-                val2 = tmp;
-            }
+            else if (num1 == 0) {
+                if (num2 == 1) {
 
-        }
-        //    printf("cur1 = %d cur2 = %d now = %d val1 = %d val2 = %d\n", cur1, cur2, now, val1, val2);
-            now++;
-            cur1++;
-        }
-       // printf("cur1 = %d cur2 = %d now = %d val1 = %d val2 = %d\n", cur1, cur2, now, val1, val2);
-        while (cur1 <= nums1Size && now <= flag2) {   //cur1 没走完 并且还未到中位数
-            tmp1 = nums1[cur1 - 1];
-            if (now == flag1) {
-                val1 = tmp1;
+                }
+                else {
+
+                }
             }
-            if (now == flag2) {
-                val2 = tmp1;
-                break;
-            }
-            now++;
-            cur1++;
-        }
-        while (cur2 <= nums2Size && now <= flag2) {   //cur2 没走完 并且还未到中位数
-            tmp2 = nums2[cur2 - 1];
-            if (now == flag1) {
-                val1 = tmp2;
-            }
-            if (now == flag2) {
-                val2 = tmp2;
-                break;
-            }
-            now++;
-            cur2++;
-        }
+    
     }
+    
     ret = (val1 + val2) / 2.0;
     return ret;
 }
