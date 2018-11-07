23817e91e5bdddbca51bb9e8a205f04fa8ade598
Mmmmmmi <812632545@qq.com>
2018-11-07 23:59:00 +0800

    11.07

--- c/InterView/ThreeSum.c
+++ c/InterView/ThreeSum.c
@@ -87,7 +87,15 @@ int** threeSum(int* nums, int numsSize, int* returnSize)
 }
 
 
-
+void Print(int **array[], int size)
+{
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("array[%d][%d] == %d ", i, j, (array[i])[j]);
+        }
+       printf("\n"); 
+    }
+}
 
 
 
