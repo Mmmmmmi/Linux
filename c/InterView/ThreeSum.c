e9984e859d42fecf26eddb021c90f6aefb2cb96f
Mmmmmmi <812632545@qq.com>
2018-11-08 23:03:14 +0800

    11.08

--- c/InterView/ThreeSum.c
+++ c/InterView/ThreeSum.c
@@ -12,13 +12,35 @@ void Swap(int *a, int *b) {
     *b = tmp;
 }
 
-void Print(int *array, int size)
+
+void Print(int *array[], int size)
+{
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("array[%d][%d] == %d ", i, j, (array[i])[j]);
+        }
+       printf("\n"); 
+    }
+}
+
+void Print1(int array[], int size)
 {
     for (int i = 0; i < size; i++) {
         printf("%d ", array[i]);
     }
     printf("\n");
 }
+
+int Search(int **array, int size, int min, int max)
+{
+    for (int i = 0; i < size; i++) {
+        if (array[i][0] == min && array[i][2] == max) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 
 int** threeSum(int* nums, int numsSize, int* returnSize)
 {
@@ -49,24 +71,30 @@ int** threeSum(int* nums, int numsSize, int* returnSize)
         }
     }
 
-
-   rettmp = (int **) malloc (sizeof(int *) * numsSize);
+    Print1(nums, numsSize);
+    rettmp = (int **) malloc (sizeof(int *) * numsSize);
     if (rettmp == NULL) {
         returnSize = 0;
         return NULL;
     }
+    i = 0;
     while (cur < numsSize - 2 && nums[cur] <= 0) {
         next = cur + 1;
         while(next < numsSize - 1) {
             jmp = next + 1;
             while(jmp < numsSize) {     
                 if (nums[cur] + nums[next] + nums[jmp] == 0) {                //如果满足条件就存储起来
+                    printf("nums[cur] == %d nums[next] == %d nums[jmp] == %d\n", nums[cur], nums[next], nums[jmp]);
+                    if (i != 0 && Search(rettmp, i, nums[cur], nums[jmp])) {
+                        jmp++;
+                        continue;
+                    }
                     (rettmp[i]) = (int *) malloc (sizeof(int) * 3);
                     (rettmp[i])[0] = nums[cur];
                     (rettmp[i])[1] = nums[next];
                     (rettmp[i])[2] = nums[jmp];
-                    printf("nums[cur] == %d nums[next] == %d nums[jmp] == %d\n", nums[cur], nums[next], nums[jmp]);
-                    retnum++;
+                    printf("array[%d][0] == %d array[%d][1] == %d array[%d][2] == %d \n", i, rettmp[i][0], i, rettmp[i][1], i, rettmp[i][2]);
+                    i++;
                 }
                 jmp++;
             }
@@ -74,37 +102,29 @@ int** threeSum(int* nums, int numsSize, int* returnSize)
         }
         cur++;
     }
-    printf("returnnum = %d\n", retnum);
-
-    if (retnum > 0) {         //有满足条件的
-        ret = (int **) malloc (sizeof(int *) * retnum);
-        memcpy(ret, rettmp, retnum);
-        *returnSize = retnum;
+    *returnSize = i;
+    if (i > 0) {
+        ret = (int **) malloc (sizeof(int *) * i);
+        memcpy(ret, rettmp, sizeof(int *) * i);
     }
+//    Print(ret, retnum);
     free(rettmp);
-
     return ret;  
 }
 
 
-void Print(int **array[], int size)
-{
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("array[%d][%d] == %d ", i, j, (array[i])[j]);
-        }
-       printf("\n"); 
-    }
-}
 
 
 
 int main()
 {
-    int num[] = {-1, 0, 1, 2, -1, -4};
+    int num[] = {-7,-4,-6,6,4,-6,-9,-10,-7,5,3,-1,-5,8,-1,-2,-8,-1,5,-3,-5,4,2,-5,-4,4,7};
+//    int num[] = {-1,-2,-3,4,1,3,0,3,-2,1,-2,2,-1,1,-5,4,-3};
+//    int num[] = {-1, 0, 1, 2, -1, -4};
     int returnSize = 0;
     int size = sizeof(num) / sizeof(num[0]);
-    threeSum(num, size, &returnSize);
+    int ** ret = threeSum(num, size, &returnSize);
+    Print(ret,returnSize);
     return 0;
 }
 
