104c69e28fd73dc9d46e7147921f76ad03c3e928
Mmmmmmi <812632545@qq.com>
2018-12-19 23:10:35 +0800

    12.19

--- c/MemorAllocation/head.h
+++ c/MemorAllocation/head.h
@@ -34,24 +34,127 @@ struct memLinkList {
 };
 
 //创建节点
-struct Node* creatNode(DataType *data)
+//每个节点里面的数据域应该是起始地址和大小
+//
+// struct Node {
+//    struct memeryNode _data;
+//    struct Node* _next;
+//    struct Node* _prev;
+//};
+struct Node* creatNode(size_t begin, size_t size)
 {
     struct Node *temp = (struct Node*) malloc (sizeof(struct Node));
+    //确保malloc 成功
     assert(temp != NULL);
-    temp->_data = *data;
+    temp->_data._begin = begin;
+    temp->_data._size = size;
+    temp->_data._end = begin + size;
     temp->_next = NULL;
     return temp;
 }
 
-//初始化
-void memListInit(struct memLinkList *pmemlist)
+//输出链表
+void printList(struct memLinkList *pmemlist)
 {
+    assert(pmemlist != NULL);
+    struct Node *cur = NULL;
+    //输出空闲链表
+    cur = pmemlist->_emptylist._head;
+    printf("空闲内存链表为：\n");
+    while (cur != NULL) {
+        printf("begin:%lu    ", cur->_data._begin);
+        cur = cur->_next;
+    }
+    printf("\n");
+    cur = pmemlist->_emptylist._head;
+    while (cur != NULL) {
+        printf("size :%lu", cur->_data._size);
+        cur = cur->_next;
+        if (cur != NULL) {
+            printf("----->");
+        }
+    }
+    printf("\n");
+    cur = pmemlist->_emptylist._head;
+    while (cur != NULL) {
+        printf("end  :%lu    ", cur->_data._end);
+        cur = cur->_next;
+    }
+    printf("\n");
+    //输出已分配内存链表
+    cur = pmemlist->_usedlist._head;
+    printf("已分配内存链表为：\n");
+    while (cur != NULL) {
+        printf("begin:%lu", cur->_data._begin);
+        cur = cur->_next;
+    }
+    printf("\n");
+    cur = pmemlist->_usedlist._head;
+    while (cur != NULL) {
+        printf("size :%lu\n", cur->_data._size);
+        cur = cur->_next;
+        if (cur != NULL) {
+            printf("----->");
+        }
+    }
+    printf("\n");
+    cur = pmemlist->_usedlist._head;
+    while (cur != NULL) {
+        printf("end  :%lu\n", cur->_data._end);
+        cur = cur->_next;
+    }
+    printf("\n");
+}
+
+//初始化 
+//需要初始化　内存的开始地址　和 大小
+void memListInit(struct memLinkList *pmemlist, size_t begin, size_t size)
+{
+    assert(pmemlist != NULL);
     pmemlist->_emptylist._head = NULL;
     pmemlist->_emptylist._tail = NULL;
     pmemlist->_usedlist._head = NULL;
     pmemlist->_usedlist._tail = NULL;
+    //先把第一个空的挂在空闲内存链表的第一个
+    struct Node *ret = creatNode(begin, size);
+    pmemlist->_emptylist._head = ret;
+    printList(pmemlist);
 }
 
+void Swap(struct Node *pnode1, struct Node *pnode2) 
+{
+    assert(pnode1 != NULL);
+    assert(pnode2 != NULL);
+    struct Node temp;
+     = pnode1->_data;
+
+}
+
+
+void sortAdr(struct memLinkList *pmemlist)
+{
+    assert(pmemlist != NULL); 
+    struct Node *cur = NULL;
+    cur = pmemlist->_emptylist._head;
+    while(cur->_next != NULL) {
+        if (cur->_data._begin > cur->_next->_data._begin) {
+            Swap(cur->_data, cur->_next->_data);
+        }
+       cur = cur->_next; 
+    }
+}
+
+
+void sortAscSize(struct memLinkList *pmemlist)
+{
+    assert(pmemlist != NULL);
+}
+
+
+void sortDescSize(struct memLinkList *pmemlist)
+{
+    assert(pmemlist != NULL);
+}
 
 void emptyMemeryPushBack(struct memLinkList *pmemlist, size_t memnumber)
 {
--- c/MemorAllocation/memoryAllocation.c
+++ c/MemorAllocation/memoryAllocation.c
@@ -1,19 +1,61 @@
-
 #include "head.h"
 #include <stdio.h>
 
-
-void FF() {
-    
+//首次适应算法
+void FF(struct memLinkList *pmemlist)
+{
+    assert(pmemlist != NULL);
+    sortAdr(pmemlist);
 }
 
-void allocateMemory(struct memLinkList *memlist)
+//循环首次适应算法
+void NF(struct memLinkList *pmemlist)
+{
+   assert(pmemlist != NULL);
+   sortAdr(pmemlist);
+}
+
+//最佳适应算法
+void BF(struct memLinkList *pmemlist)
+{
+   assert(pmemlist != NULL);
+   sortAscSize(pmemlist); 
+}
+
+//最坏适应算法
+void WF(struct memLinkList *pmemlist)
+{
+   assert(pmemlist != NULL);
+   sortAscSize(pmemlist); 
+}
+
+void allocateMemory(struct memLinkList *pmemlist)
 {
     int i;
     printf("请输入选项");
     printf("1.首次适应算法  2.循环首次适应算法  3.最佳适应算法  4.最坏适应算法 :");
-    scanf("%d", &i);
-
+    scanf("%d", &i);       //没有进行输入错误处理，直接当输入的正确
+    switch(i) {
+        case 1:
+            //首次适应算法
+            FF(pmemlist);
+            break;
+        case 2:
+            //循环首次适应算法
+            NF(pmemlist);
+            break;
+        case 3:
+            //最佳适应算法
+            BF(pmemlist);
+            break;
+        case 4:
+            //最坏适应算法
+            WF(pmemlist);
+            break;
+        default:
+            printf("输入错误！\n");
+            break;
+    }
 }
 
 void recoveryMemory(struct memLinkList *memlist)
@@ -29,23 +71,27 @@ int main()
     struct memLinkList memlist;
     printf("please input memery begin: ");
     scanf("%lu", &membegin);
-    printf("please input memery szie: ");
+    printf("please input memery szie : ");
     scanf("%lu", &memsize);
     memListInit(&memlist, membegin, memsize);
-
     while (1) {
-            printf("请选择功能: ");
-            printf("1.分配空间    2.回收空间: ");
-            scanf("%d", &i);
-            switch(i) {
-            case 1:
-                allocateMemory(&memlist);
-            case 2:
-                recoveryMemory(&memlist);
-                
-            }
+        printf("请选择功能: ");
+        printf("1.分配空间    2.回收空间   3.退出: ");
+        scanf("%d", &i);
+        switch(i) {
+        case 1:
+            allocateMemory(&memlist);
+            break;
+        case 2:
+            recoveryMemory(&memlist);
+            break;
+        case 3:
+            printf("程序退出！\n");
+            return 0;
+        default:
+            printf("选项输入有误，请重新输入！\n");
+            break;
         }
-
-        return 0;
+    }
+    return 0;
 }
-
