b417a1d5538009a71e87a201892a1f0519f0e7a1
Mmmmmmi <812632545@qq.com>
2018-12-29 21:47:46 +0800

    12.29

--- c++/BankersAlgorithm/head.h
+++ c++/BankersAlgorithm/head.h
@@ -2,16 +2,30 @@
 #define __HEAD_H__
 
 
-
-class head
+#include <vector>
+#include <istream>
+using namespace std;
+//进程对象
+class press
 {
 public:
-    head() {}
-    ~head() {}
-
+    press() {}
+    ~press() {}
 private:
-
+    vector<vector<int>> _max;           //最大需求
+    vector<vector<int>> _allocation;    //已分配的    
+    vector<vector<int>> _need;          //尚需要的
+    vector<vector<int>> _request;       //本次请求的   
 };
 
+//系统中的资源对象
+class resources
+{
+public:
+    resources() {}
+    ~resources() {}
+private:
+    vector<vector<int>> _available;     //可利用的资源数目
+};
 
 #endif
